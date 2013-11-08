eea9d75a18306271cd85df5dad9773723388655f
Rizwana Qadir <Rizwana Qadir@192.168.1.233>
2013-11-08 14:30:47 +0100

    In der class Map erst mal ein zweidimensionales Array ([5][13]) erstellt, welches dann zur Darstellung der (kugelförmigen) Marsoberfläche dienen soll.

---
+++ src/Map.java
@@ -0,0 +1,7 @@
+import java.util.Observer;
+import java.util.Observable;
+import java.math.*;
+
+public class Map {
+	int [][] a = new int [5][13]; // ein Array mit 5 Zeilen und 13 Spalten für die Darstellung der Marsoberfläche.
+}
---
+++ src/MoveRoverTest.java
@@ -0,0 +1,18 @@
+import java.awt.event.KeyEvent;
+
+import org.junit.Test;
+
+
+public class MoveRoverTest extends junit.framework.TestCase {
+
+	@Test
+	public void test () {
+		Rover MarsRover = new Rover();
+		Map MarsMap = new Map();
+	}
+	public void KeyPressed (KeyEvent e){
+	}
+	
+	}
+
+
