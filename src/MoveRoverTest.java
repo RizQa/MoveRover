c17392e6f21d979b81383e66bdf7c6baa923a187
Rizwana Qadir <Rizwana Qadir@192.168.1.233>
2013-11-09 19:18:22 +0100

    Ich habe den Test versucht zu erweitern, indem versuche den Rover mit den Tasten "up, down, left und right" (für die entsprechenden Himmelsrichtungen ) zu steuern.
    weiteres im nächsten Commit :D

--- src/MoveRoverTest.java
+++ src/MoveRoverTest.java
@@ -1,5 +1,4 @@
 import java.awt.event.KeyEvent;
-
 import org.junit.Test;
 
 
@@ -11,8 +10,24 @@ public class MoveRoverTest extends junit.framework.TestCase {
 		Map MarsMap = new Map();
 	}
 	public void KeyPressed (KeyEvent e){
-	}
+		switch (e.getKeyCode())
+		{
+		case KeyEvent.VK_UP:			//North
+			move.setLocation ((int)move.getLocation().getX(), (int)move.getLocation().getY()+30);	
+			break;
+			
+		case KeyEvent.VK_DOWN:			//South
+			move.setLocation ((int)move.getLocation().getX(), (int)move.getLocation().getY()-30);
+			break;
+		case KeyEvent.VK_LEFT:			//West
+			move.setLocation ((int)move.getLocation().getX()-30, (int)move.getLocation().getY());
+			break;
+		case KeyEvent.VK_RIGHT: 		//East
+			move.setLocation ((int)move.getLocation().getX()+30, (int)move.getLocation().getY());
+			break;
+		}
 	
 	}
+}
 
 
